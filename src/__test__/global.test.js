b1721b50cd4d1b8dc3fb7dcf990c9b80d1b12ef7
Freddy Moreno <freddy.moreno@gmail.com>
2022-05-17 20:54:04 -0400

    Implementando pruebas a promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -26,3 +26,43 @@ test('Probar un Callback', () => {
         expect(str).toBe('aloH');
     });
 });
+
+const reverseString2 = str => {
+    return new Promise((resolve, reject) => {
+        if (!str) {
+            reject(new Error('No existe el string'));
+        }
+        if (typeof str !== 'string') {
+            reject(new Error('El parametro no es un string'));
+        }
+        resolve(str.split('').reverse().join(''));
+    });
+}
+
+test('Probar una promesa', () => {
+    return reverseString2('Hola')
+      .then(string => {
+        expect(string).toBe('aloH');
+    });
+});
+
+test('Probar async/await', async () => {
+    const string = await reverseString2('Hola');
+    expect(string).toBe('aloH');
+});
+
+afterEach(() => {
+    console.log('Despues de cada test');
+});
+
+afterAll(() => {
+    console.log('Despues de todos los test');
+});
+
+beforeEach(() => {
+    console.log('Antes de cada test');
+});
+
+beforeAll(() => {
+    console.log('Antes de todos los test');
+});
